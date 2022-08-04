fb78c8ded0ab7b9d1b8e203f9648bbeefd30a90e
RudeGalaxy1010 <Karev007@bk.ru>
2022-08-04 19:55:51 +0300

    Sellzone pulsation animation

--- Assets/Scripts/SellZone.cs
+++ Assets/Scripts/SellZone.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using DG.Tweening;
 
 namespace IdleActionFarm
 {
     [RequireComponent(typeof(Collider))]
     public class SellZone : MonoBehaviour
     {
+        [SerializeField] private float _waveDuration;
         [SerializeField] private Transform _stacksPoint;
+
+        private void Start()
+        {
+            Material material = GetComponent<Renderer>().material;
+            var targetColor = new Color(material.color.r, material.color.g, material.color.b, 0);
+            material.DOColor(targetColor, _waveDuration / 2f).From().SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        }
 
         private void OnTriggerEnter(Collider other)
         {
